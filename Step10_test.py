50144e6a18f21cfbbd530eb7f588755ae80cdd4e
leejiyoon <ktcloudljy@gmail.com>
2026-03-20 12:05:22 +0900

    Step10 추가함

---
+++ Step10_test.py
@@ -0,0 +1,12 @@
+
+from Step10_main import TestClass
+
+
+if __name__ == "__main__":
+    # TestClass() 로 객체(인스턴스) 생성하기
+    a : TestClass = TestClass()
+    print("종료됩니다.")
+
+
+if __name__ == "__main__" :
+  pass
